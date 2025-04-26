fb098687681e5bf425d465592a641132162933d5
Mayank Vishwakarma <mayankvishw09@gmail.com>
2025-04-26 17:39:30 +0530

    `Added react-router-dom and related dependencies for routing`

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 };
